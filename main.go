aaa6262f2efc30524d46c5ea447de48fc78d7663
bricefrisco <bricemfrisco@gmail.com>
2025-07-10 11:39:54 -0500

    Remove 'hello' cron job

--- main.go
+++ main.go
@@ -8,11 +8,6 @@ import (
 func main() {
 	app := pocketbase.New()
 
-	// prints "Hello!" every 2 minutes
-	app.Cron().MustAdd("hello", "*/2 * * * *", func() {
-		log.Println("Hello!")
-	})
-
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
