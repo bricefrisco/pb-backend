58c2d14701a38cee5928f0c35f82cbcfaf410031
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2025-10-17 22:08:16 -0500

    reduce maxIterations to 20 (1000 battles)

--- battleboards.go
+++ battleboards.go
@@ -25,7 +25,7 @@ func NewBattleboards(app *pocketbase.PocketBase) *Battleboards {
 		app:           app,
 		albionAPI:     NewAlbionAPI(),
 		queue:         make(chan queueItem, 100),
-		maxIterations: 100,
+		maxIterations: 20,
 	}
 }
 
