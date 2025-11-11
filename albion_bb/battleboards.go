6d6f3166103dfffd1b286859f7247afcdd51de74
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2025-11-10 19:32:36 -0600

    archive

--- albion_bb/battleboards.go
+++ albion_bb/battleboards.go
@@ -1,4 +1,4 @@
-package main
+package albion_bb
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -1,74 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/tools/cron"
-	sCron "github.com/robfig/cron/v3"
 	"log"
 )
 
-type Backend struct {
-	app          *pocketbase.PocketBase
-	cron         *cron.Cron
-	battleboards *Battleboards
-}
-
-func NewBackend() *Backend {
+func main() {
 	app := pocketbase.New()
-	return &Backend{
-		app:          app,
-		cron:         cron.New(),
-		battleboards: NewBattleboards(app),
-	}
-}
-
-func (b *Backend) Run() {
-	b.cron.Start()
-	if err := b.app.Start(); err != nil {
+	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func main() {
-	backend := NewBackend()
-
-	backend.app.OnServe().BindFunc(func(e *core.ServeEvent) error {
-		c := sCron.New(
-			sCron.WithSeconds(),
-			sCron.WithChain(
-				sCron.SkipIfStillRunning(sCron.DefaultLogger),
-			),
-		)
-
-		_, err := c.AddFunc("10 * * * * *", func() {
-			fmt.Println("Scheduler started")
-			err := backend.battleboards.FetchNewBattles()
-			if err != nil {
-				fmt.Println("Error with cron job (FetchNewBattles):", err)
-			}
-
-			err = backend.battleboards.EnqueueNewBattles()
-			if err != nil {
-				fmt.Println("Error with cron job (EnqueueNewBattles):", err)
-			}
-			fmt.Println("Scheduler finished")
-		})
-
-		c.Start()
-
-		if err != nil {
-			fmt.Println("Failed to schedule cron job:", err)
-		}
-
-		go func() {
-			backend.battleboards.ProcessQueue()
-		}()
-
-		return e.Next()
-	})
-
-	fmt.Println("Starting backend...")
-	backend.Run()
 }
