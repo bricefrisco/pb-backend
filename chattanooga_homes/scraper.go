df3a81e35ad97e8b7d3667603fe27df87fccd3b1
Brice Frisco <bricemfrisco@gmail.com>
2025-12-09 07:26:20 -0600

    add marion county

--- chattanooga_homes/scraper.go
+++ chattanooga_homes/scraper.go
@@ -18,7 +18,7 @@ const (
 	baseURL = "https://my.flexmls.com/greaterchattanooganew/search/idx_links/20240916004638701725000000/listings"
 
 	// Default filter for Chattanooga area homes (only Active/Pending/Contingent)
-	defaultFilter = "MlsId+Eq+'20240417141107895724000000'+And+CountyOrParish+Eq+'Hamilton','Marion','Sequatchie',+And+MlsStatus+Eq+'Active','Pending','Contingent'+And+PropertyType+Eq+'A'+And+CurrentPrice+Bt+250000.0,800000.0+And+\"General+Property+Information\".\"Lot+Size+Acres\"+Ge+2.0"
+	defaultFilter = "MlsId+Eq+'20240417141107895724000000'+And+CountyOrParish+Eq+'Hamilton','Marion','Sequatchie'+And+MlsStatus+Eq+'Active','Pending','Contingent'+And+PropertyType+Eq+'A'+And+CurrentPrice+Bt+250000.0,800000.0+And+\"General+Property+Information\".\"Lot+Size+Acres\"+Ge+2.0"
 
 	// Pagination settings
 	pageLimit = 10 // listings per page
